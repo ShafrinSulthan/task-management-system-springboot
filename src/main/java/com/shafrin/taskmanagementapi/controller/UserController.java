95e98b4701b761c924544c437171f12fdff57e1f
Shafrin <shafrinsulthan03@gmail.com>
2026-06-05 13:50:03 +0530

    Added User API

---
+++ src/main/java/com/shafrin/taskmanagementapi/controller/UserController.java
@@ -0,0 +1,24 @@
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shafrin.taskmanagementapi.entity.User;
+import com.shafrin.taskmanagementapi.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController 
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
